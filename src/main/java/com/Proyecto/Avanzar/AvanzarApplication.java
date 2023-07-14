c56f2af28eaa57267fc525884fd0f451ba645c9a
DavidSisalima7 <97250207+DavidSisalima7@users.noreply.github.com>
2023-07-14 09:05:18 -0500

    creacion de proyecto

---
+++ src/main/java/com/Proyecto/Avanzar/AvanzarApplication.java
@@ -0,0 +1,13 @@
+package com.Proyecto.Avanzar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvanzarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvanzarApplication.class, args);
+	}
+
+}
