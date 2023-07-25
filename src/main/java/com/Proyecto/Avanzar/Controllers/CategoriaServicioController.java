f14b40007c0b7057cc47d9f3a25e441c3b87516c
kipu1 <israel.ortiz.est@tecazuay.edu.ec>
2023-07-25 16:49:48 -0500

    d

--- src/main/java/com/Proyecto/Avanzar/Controllers/CategoriaServicioController.java
+++ src/main/java/com/Proyecto/Avanzar/Controllers/CategoriaServicioController.java
@@ -1,10 +1,14 @@
 package com.Proyecto.Avanzar.Controllers;
 
+
+import com.Proyecto.Avanzar.Models.CategoriaServicio;
 import com.Proyecto.Avanzar.Services.service.CategoriaServicioService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin(origins = { "*" })
 @RestController
@@ -12,5 +16,43 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoriaServicioController {
     @Autowired
     CategoriaServicioService categoriaServicioService;
+    @PostMapping("/registrar")
+    public ResponseEntity<CategoriaServicio> crear(@RequestBody CategoriaServicio r) {
+        try {
+            return new ResponseEntity<>(categoriaServicioService.save(r), HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 
+    @GetMapping("/listar")
+    public ResponseEntity<List<CategoriaServicio>> obtenerLista() {
+        try {
+            return new ResponseEntity<>(categoriaServicioService.findByAll(), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @DeleteMapping("/eliminar/{id}")
+    public ResponseEntity<?> eliminar(@PathVariable Long id, @RequestBody CategoriaServicio categoriaServicio) {
+        return categoriaServicioService.delete(id);
+    }
+
+    @PutMapping("/actualizar/{id}")
+    public ResponseEntity<CategoriaServicio> actualizar(@PathVariable Long id,@RequestBody CategoriaServicio p) {
+        CategoriaServicio categoriaServicio = categoriaServicioService.findById(id);
+        if (categoriaServicio == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else {
+            try {
+                categoriaServicio.setNombreCategoria(p.getNombreCategoria());
+                categoriaServicio.setDescripcion(p.getDescripcion());
+                categoriaServicio.setEstado(p.getEstado());
+                return new ResponseEntity<>(categoriaServicioService.save(categoriaServicio), HttpStatus.CREATED);
+            } catch (Exception e) {
+                return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+
+        }
+    }
 }
--- src/main/java/com/Proyecto/Avanzar/Controllers/LikesController.java
+++ src/main/java/com/Proyecto/Avanzar/Controllers/LikesController.java
@@ -1,10 +1,13 @@
 package com.Proyecto.Avanzar.Controllers;
 
+import com.Proyecto.Avanzar.Models.Likes;
 import com.Proyecto.Avanzar.Services.service.LikesService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin(origins = { "*" })
 @RestController
@@ -12,4 +15,43 @@ import org.springframework.web.bind.annotation.RestController;
 public class LikesController {
     @Autowired
     LikesService likesService;
+    @PostMapping("/registrar")
+    public ResponseEntity<Likes> crear(@RequestBody Likes r) {
+        try {
+            return new ResponseEntity<>(likesService.save(r), HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/listar")
+    public ResponseEntity<List<Likes>> obtenerLista() {
+        try {
+            return new ResponseEntity<>(likesService.findByAll(), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @DeleteMapping("/eliminar/{id}")
+    public ResponseEntity<?> eliminar(@PathVariable Long id, @RequestBody Likes likes) {
+        return likesService.delete(id);
+    }
+
+    @PutMapping("/actualizar/{id}")
+    public ResponseEntity<Likes> actualizar(@PathVariable Long id,@RequestBody Likes p) {
+        Likes likes = likesService.findById(id);
+        if (likes == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else {
+            try {
+                likes.setFecha(p.getFecha());
+
+
+                return new ResponseEntity<>(likesService.save(likes), HttpStatus.CREATED);
+            } catch (Exception e) {
+                return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+
+        }
+    }
 }
