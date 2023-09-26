4e27dcbf8df6c2b851c7e574a4121600063d29b0
Chimbo-K18 <mateinochimbo18@gmail.com>
2023-09-26 00:16:49 -0500

    Ahora Full

--- src/main/java/com/Proyecto/Avanzar/Repository/PublicacionesRepository.java
+++ src/main/java/com/Proyecto/Avanzar/Repository/PublicacionesRepository.java
@@ -12,7 +12,7 @@ public interface PublicacionesRepository extends JpaRepository<Publicaciones, Lo
 
     //Query para listar publicaciones activas
     @Query(value = "SELECT *\n" +
-            "\tFROM publicaciones WHERE visible=true order by fecha_publicacion desc", nativeQuery = true)
+            "\tFROM publicaciones WHERE estado=true order by fecha_publicacion desc", nativeQuery = true)
     public List<Publicaciones> listar();
 
     @Query(value = "SELECT * FROM publicaciones WHERE productos_id_producto IS NOT NULL AND visible=true AND vendedor_id_vendedor = :vendedorId ORDER BY fecha_publicacion DESC", nativeQuery = true)
@@ -33,10 +33,10 @@ public interface PublicacionesRepository extends JpaRepository<Publicaciones, Lo
     public List<Publicaciones> listarPublicacionesConServicios(@Param("vendedorId") Long vendedorId);
 
 
-    @Query(value = "SELECT * FROM publicaciones WHERE servicios_id_servicio IS NOT NULL AND visible=true ORDER BY fecha_publicacion DESC", nativeQuery = true)
+    @Query(value = "SELECT * FROM publicaciones WHERE servicios_id_servicio IS NOT NULL AND estado=true ORDER BY fecha_publicacion DESC", nativeQuery = true)
     public List<Publicaciones> listarServicios();
 
-    @Query(value = "SELECT * FROM publicaciones WHERE productos_id_producto IS NOT NULL AND visible=true ORDER BY fecha_publicacion DESC", nativeQuery = true)
+    @Query(value = "SELECT * FROM publicaciones WHERE productos_id_producto IS NOT NULL AND estado=true ORDER BY fecha_publicacion DESC", nativeQuery = true)
     public List<Publicaciones> listarProductos();
     
     @Query(value="SELECT * FROM publicaciones p JOIN destacados d ON p.id_publicacion=d.publicaciones_id_publicacion AND d.usuario_id = :idUser", nativeQuery = true)
