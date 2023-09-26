955ff0ed803824354a5d1519a80772d78dc8c09c
DavidSisalima7 <97250207+DavidSisalima7@users.noreply.github.com>
2023-09-25 21:27:40 -0500

    update publicaciones en orden de fecha

--- src/main/java/com/Proyecto/Avanzar/Repository/PublicacionesRepository.java
+++ src/main/java/com/Proyecto/Avanzar/Repository/PublicacionesRepository.java
@@ -12,7 +12,7 @@ public interface PublicacionesRepository extends JpaRepository<Publicaciones, Lo
 
     //Query para listar publicaciones activas
     @Query(value = "SELECT *\n" +
-            "\tFROM publicaciones WHERE visible=true", nativeQuery = true)
+            "\tFROM publicaciones WHERE visible=true order by fecha_publicacion desc", nativeQuery = true)
     public List<Publicaciones> listar();
 
     @Query(value = "SELECT * FROM publicaciones WHERE productos_id_producto IS NOT NULL AND visible=true AND vendedor_id_vendedor = :vendedorId ORDER BY fecha_publicacion DESC", nativeQuery = true)
